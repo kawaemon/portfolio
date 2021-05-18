2a0419ef3f8cfd0e29528f2b0eb74f7c64f4a47b
kawaemon <me@kawaemon.dev>
2021-05-19 05:48:18 +0900

    feat: Footer and copyright notice

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+export const Footer: FC = () => (
+    <footer className="max-w-screen-md mx-auto text-center my-8 text-gray-500">
+        Copyright since 2021 かわえもん All rights reserved.
+    </footer>
+);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,19 +4,20 @@ import { Header } from "../components/Header";
 import { Profile } from "../components/Profile";
 import { Skills } from "../components/Skills";
 import { History } from "../components/History";
+import { Footer } from "../components/Footer";
 
 const Index: FC = () => (
-    <>
+    <article className="bg-gray-900 text-white w-full">
         <Header />
 
-        <main className="bg-gray-900 text-white w-full">
-            <article>
-                <Profile />
-                <Skills />
-                <History />
-            </article>
+        <main>
+            <Profile />
+            <Skills />
+            <History />
         </main>
-    </>
+
+        <Footer />
+    </article>
 );
 
 export default Index;
