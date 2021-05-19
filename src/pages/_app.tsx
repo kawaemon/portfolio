e1cf2cbc20e83bb08e42e47344fa84ce0a637ef8
kawaemon <me@kawaemon.dev>
2021-05-19 19:29:16 +0900

    feat: add meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,11 +12,22 @@ const App = ({ Component, pageProps }: AppProps) => (
             />
 
             <title>かわえもんのポートフォリオ</title>
+
             <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link
                 rel="stylesheet"
                 href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500&display=swap"
             />
+
+            <meta name="name" content="kawaemon's portfolio" />
+            <meta name="description" content="kawaemon describes himself" />
+
+            <meta property="og:title" content="kawaemon's portfolio" />
+            <meta
+                property="og:description"
+                content="kawaemon describes himself"
+            />
+            <meta property="og:locale" content="ja_JP" />
         </Head>
 
         <Component {...pageProps} />
