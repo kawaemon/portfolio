60275914f109ae911b680aea64064ee505717679
kawaemon <me@kawaemon.dev>
2021-05-20 15:15:01 +0900

    style: make footer smaller

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 
 export const Footer: FC = () => (
-    <footer className="text-center my-8 text-gray-500">
+    <footer className="text-center text-xs my-8 text-gray-500">
         Copyright since 2021 かわえもん All rights reserved.
     </footer>
 );
