d726a3067fd69d19bfda084f27835728299cc3ca
kawaemon <me@kawaemon.dev>
2023-12-27 23:47:52 +0900

    fix: sometimes onAnimtionEnd does not fire

--- app/components/HeaderLogo.tsx
+++ app/components/HeaderLogo.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import styles from "./Header.module.css";
 
 // set-if-true
@@ -11,28 +11,37 @@ function sit(shouldSpecify: boolean, value: string): string {
     return "";
 }
 
+type AnimationState = "init" | "animating" | "done";
+
 export const HeaderLogo: FC = () => {
-    const [isHoverAnimating, setHoverAnimating] = useState(false);
+    const [s, ss] = useState<AnimationState>("init");
+
+    useEffect(() => {
+        if (s === "done") {
+            ss("init");
+        }
+    }, [s]);
+
+    const begin = () => ss((x) => (x === "init" ? "animating" : x));
 
     return (
-        <svg className={`w-48 h-48 md:w-64 md:h-64`} viewBox="0 0 100 100">
+        <svg
+            className="w-48 h-48 md:w-64 md:h-64"
+            viewBox="0 0 100 100"
+            onMouseEnter={begin}
+            onClick={begin}
+        >
             <g className={`${styles.animated} ${styles.logo}`}>
                 <polygon
                     className={[
                         styles.animated,
-                        sit(isHoverAnimating, styles["rotate-on-hover"]),
+                        sit(s === "animating", styles["rotate-on-hover"]),
                     ].join(" ")}
-                    onMouseEnter={() => {
-                        console.log("hover");
-                        setHoverAnimating(true);
-                    }}
-                    onAnimationEnd={() => {
-                        setHoverAnimating(false);
-                    }}
                     points="50,5 95,50 50,95 5,50"
                     stroke="white"
                     strokeWidth="1"
                     fill="none"
+                    onAnimationEnd={() => ss("done")}
                 />
             </g>
         </svg>
