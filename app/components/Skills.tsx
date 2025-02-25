cda05b8f715917ba92ba90f7ea350d12ffd69629
kawaemon <me@kawaemon.dev>
2025-02-26 04:24:51 +0900

    feat: new skills

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,165 +1,115 @@
 import React, { FC, ReactNode } from "react";
-import { SkillsTooltip } from "./SkillsTooltip";
+import styles from "./Skills.module.css";
+import { ExternalLink } from "./base/ExternalLink";
 
-type SkillLevel = "fine" | "learning";
+// eslint-disable-next-line @typescript-eslint/no-namespace
+namespace Card {
+    export type Props = {
+        children: ReactNode;
+    };
+}
 
-const skillLevelToColor = (l: SkillLevel) => {
-    switch (l) {
-        case "fine":
-            return "text-green-400";
-        case "learning":
-            return "text-orange-400";
-    }
-};
-
-const ColoredCircle: FC<{ color?: string }> = ({ color }) => (
-    <svg className={`inline w-4 mr-2 ${color ?? ""}`} viewBox="0 0 100 100">
-        <circle cx="50" cy="50" r="50" fill="currentColor" />
-    </svg>
-);
-
-const SkillEntry: FC<{ level: SkillLevel; children: ReactNode }> = (p) => {
-    const { children, level } = p;
+const Card: FC<Card.Props> = ({ children }) => {
     return (
-        <li className="inline-flex flex-row items-center border-white border-2 border-opacity-40 border-dotted rounded-full px-4 py-0.5 mr-2 mt-2">
-            <span className={`marker-${level} flex`}>
-                <ColoredCircle color={skillLevelToColor(level)} />
-            </span>
+        <section
+            className={`p-4 bg-gray-800 rounded-md w-full ${styles.cardWidth}`}
+        >
             {children}
-        </li>
+        </section>
     );
 };
-
-const dottedBorder =
-    "border-white border-2 border-opacity-40 border-dotted rounded-xl";
-
-const BorderedList: FC<{ children: ReactNode }> = ({ children }) => (
-    <li className={`${dottedBorder} px-4 py-2 mt-3`}>{children}</li>
-);
-
-// prettier-ignore
-type ArrayType<T extends ReadonlyArray<unknown>> =
-    T extends ReadonlyArray<infer U> ? U : never;
-
-type SkillsList = ReadonlyArray<{
-    sectionName: string;
-    list: ReadonlyArray<SkillsListInner>;
-}>;
-
-type SkillsListInner =
-    | { level: SkillLevel; text: string }
-    | ArrayType<SkillsList>;
-
-const isInnerSkillsList = (
-    inner: SkillsListInner,
-): inner is ArrayType<SkillsList> =>
-    (inner as Record<string, unknown>)["level"] == null;
-
-const skillsList: SkillsList = [
-    {
-        sectionName: "Web フロントエンド",
-        list: [
-            { level: "fine", text: "HTML5" },
-            { level: "fine", text: "CSS" },
-            { level: "fine", text: "TypeScript" },
-            { level: "learning", text: "Svelte" },
-            { level: "learning", text: "Solid.js" },
-            { level: "fine", text: "React" },
-            { level: "fine", text: "Next.js" },
-            { level: "fine", text: "GraphQL" },
-            { level: "fine", text: "Rust (WASM Yew)" },
-        ],
-    },
-    {
-        sectionName: "Web バックエンド",
-        list: [
-            {
-                sectionName: "Rust",
-                list: [
-                    { level: "fine", text: "Tokio" },
-                    { level: "fine", text: "Warp" },
-                    { level: "fine", text: "Axum" },
-                    { level: "fine", text: "SeaORM" },
-                    { level: "fine", text: "Sqlx" },
-                ],
-            },
-            {
-                sectionName: "TypeScript(Node.js)",
-                list: [
-                    { level: "fine", text: "Fastify" },
-                    { level: "fine", text: "Nest.js" },
-                    { level: "fine", text: "Prisma" },
-                    { level: "fine", text: "TypeORM" },
-                ],
-            },
-            { level: "fine", text: "Go" },
-            { level: "fine", text: "MongoDB" },
-            { level: "fine", text: "SQL" },
-        ],
-    },
-    {
-        sectionName: "インフラ",
-        list: [
-            { level: "fine", text: "Terraform (CDKTF)" },
-            { level: "fine", text: "AWS CDK" },
-        ],
-    },
-    {
-        sectionName: "組み込み",
-        list: [
-            { level: "fine", text: "C (AVR, PIC)" },
-            { level: "fine", text: "ASM (PIC)" },
-            { level: "fine", text: "Arduino" },
-            { level: "fine", text: "Python3 (Raspberry Pi GPIO)" },
-        ],
-    },
-    {
-        sectionName: "その他",
-        list: [
-            { level: "fine", text: "Git" },
-            { level: "fine", text: "GitHub" },
-            { level: "fine", text: "Docker" },
-            { level: "fine", text: "Vagrant" },
-        ],
-    },
-];
-
-const SkillListImpl: FC<{ entry: ArrayType<SkillsList> }> = ({ entry }) => (
-    <ul>
-        {entry.list.map((x) => {
-            if (!isInnerSkillsList(x)) {
-                return (
-                    <SkillEntry key={x.text} level={x.level}>
-                        {x.text}
-                    </SkillEntry>
-                );
-            }
-
-            return (
-                <BorderedList key={x.sectionName}>
-                    {x.sectionName}
-                    <SkillListImpl entry={x} />
-                </BorderedList>
-            );
-        })}
-    </ul>
-);
 
 export const Skills: FC = () => {
     return (
         <section className="mt-8 max-w-full">
             <h3>できること</h3>
-
-            <ul>
-                {skillsList.map((x) => (
-                    <BorderedList key={x.sectionName}>
-                        {x.sectionName}
-                        <SkillListImpl entry={x} />
-                    </BorderedList>
-                ))}
-            </ul>
-
-            <SkillsTooltip />
+            {/* できることはスキルだけの紹介にとどめて、作品紹介は別の場所に分けてタグ付けする形がいい気がするな。このままだと技術と作品が many-to-1 だとこまる。 */}
+            <div className="flex flex-wrap gap-2">
+                <Card>
+                    <h4>Rust</h4>
+                    <p className="text-gray-400">
+                        一番好んで使っている言語です。業務経験があります。
+                        ネイティブアプリをしっかり作りたいなと思ったら基本これを使っています。
+                        ツールチェイン全体の開発体験がダントツで良いです。
+                    </p>
+                    {/* https://github.com/kawaemon/isucon14/tree/revenge-master/app/rust
+                        https://github.com/approvers/rusty-ponyo */}
+                </Card>
+                <Card>
+                    <h4>OSS Contribution</h4>
+                    <p className="text-gray-400">
+                        普段から OSS
+                        のお世話になり続けているので、気づいたバグや改善には積極的に取り組むようにしています。
+                        成熟した大規模コードベースに取り組むと、いつも新たな発見があり楽しいです。
+                    </p>
+                    <p className="text-gray-400">
+                        <ExternalLink
+                            href="https://github.com/rust-lang/cargo/pull/11579"
+                            label="Cargo に投げた PR"
+                            className="underline"
+                        >
+                            Cargo
+                        </ExternalLink>
+                        {", "}
+                        <ExternalLink
+                            href="https://github.com/FFmpeg/FFmpeg/commit/61aa532e22668ae0ed943e4aee3ea69af648375e"
+                            label="ffmpeg に投げた パッチ"
+                            className="underline"
+                        >
+                            ffmpeg
+                        </ExternalLink>
+                        {", "}
+                        <ExternalLink
+                            href="https://github.com/hyperium/http/pull/741"
+                            label="hyperium/http に投げた PR"
+                            className="underline"
+                        >
+                            hyperium/http
+                        </ExternalLink>
+                        , ...
+                    </p>
+                    {/*
+                        https://github.com/hyperium/http/pull/741
+                        https://github.com/FFmpeg/FFmpeg/commit/61aa532e22668ae0ed943e4aee3ea69af648375e
+                        mailing list へのリンク欲しくないか？↑
+                        https://github.com/rustwasm/wasm-bindgen/pull/4093
+                        https://github.com/swc-project/swc-node/pull/723
+                        https://github.com/rust-lang/cargo/pull/11579 */}
+                </Card>
+                <Card>
+                    <h4>React</h4>
+                    <p className="text-gray-400">
+                        最近最前線は追えていませんが、書けはします。業務経験があります。
+                    </p>
+                    {/* https://github.com/kawaemon/portfolio */}
+                </Card>
+                <Card>
+                    <h4>Linux</h4>
+                    <p className="text-gray-400">
+                        普段から Windows, macOS, Linux
+                        を用途に応じて使い分けて使っています。 開発は主に Linux
+                        で行っているため、実用経験はあります。 git, vim, Docker
+                        等使えます。
+                    </p>
+                    {/* https://github.com/kawaemon/dotfiles/ */}
+                </Card>
+                <Card>
+                    <h4>インフラ</h4>
+                    <p className="text-gray-400">
+                        フロント, バック, RDBMS, S3
+                        等組み合わせたインフラの運用経験があります。 主に GCP,
+                        AWS を、CDK を通して使っています。
+                    </p>
+                    {/* (imei) */}
+                </Card>
+                <Card>
+                    <h4>その他</h4>
+                    <p className="text-gray-400">
+                        Go, Python, C は割とよく使います。 Java, C#, Lua
+                        は触ったことがある程度です。
+                    </p>
+                </Card>
+            </div>
         </section>
     );
 };
