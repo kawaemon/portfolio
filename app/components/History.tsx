2c94d94d52f0d0991d00f3d218194e43f43ba806
kawaemon <me@kawaemon.dev>
2025-02-25 18:23:33 +0900

    feat: reverse order history

--- app/components/History.tsx
+++ app/components/History.tsx
@@ -8,33 +8,10 @@ export const History: FC = () => (
         <H3>略歴</H3>
         <Table>
             {{
-                "2019年04月": "新居浜高専 電気情報工学科に入学",
-                "2019年09月": "学生LT 第28回 運営&登壇",
-                "2020年08月":
-                    "ProvinDevsチームとしてOpen Hack U 2020 Vol.1に出場",
-                "2020年10月": "C-Style株式会社で活動開始",
-                "2021年01月":
-                    "サポーターズ ウインターハッカソン Vol.6でチーム優勝",
-                "2021年03月":
-                    "ProvinDevsチームとしてOpen Hack U 2020 Vol.5に出場",
-                "2022年03月": "株式会社ゆめみで活動開始",
-                "2024年03月": "新居浜高専 電気情報工学科を卒業: 準学士（工学）",
-                "2024年04月": "慶應義塾大学 総合政策学部に進学",
-                "2024年12月": (
-                    <>
-                        ISUCON14 でチームスコア順位 8/834 位{" "}
-                        <ExternalLink
-                            className="underline"
-                            href="https://zenn.dev/kawaemon/articles/f40529e0c447f5"
-                            label="ISUCON14 本戦参加記へのリンク"
-                        >
-                            [記事]
-                        </ExternalLink>
-                    </>
-                ),
                 "2025年01月": (
                     <>
                         ISUCON14 感想戦で 2 位{" "}
+                        {/* やっぱりなんとかして母数書きたいよね */}
                         <ExternalLink
                             className="underline"
                             href="https://zenn.dev/kawaemon/articles/529fd6a12ac43b"
@@ -44,6 +21,30 @@ export const History: FC = () => (
                         </ExternalLink>
                     </>
                 ),
+                "2024年12月": (
+                    <>
+                        ISUCON14 でチームスコア 8/834 位{" "}
+                        <ExternalLink
+                            className="underline"
+                            href="https://zenn.dev/kawaemon/articles/f40529e0c447f5"
+                            label="ISUCON14 本戦参加記へのリンク"
+                        >
+                            [記事]
+                        </ExternalLink>
+                    </>
+                ),
+                "2024年04月": "慶應義塾大学 総合政策学部に進学",
+                "2024年03月": "新居浜高専 電気情報工学科を卒業: 準学士（工学）",
+                "2022年03月": "株式会社ゆめみで活動開始",
+                "2021年03月":
+                    "ProvinDevsチームとしてOpen Hack U 2020 Vol.5に出場",
+                "2021年01月":
+                    "サポーターズ ウインターハッカソン Vol.6でチーム優勝",
+                "2020年10月": "C-Style株式会社で活動開始",
+                "2020年08月":
+                    "ProvinDevsチームとしてOpen Hack U 2020 Vol.1に出場",
+                "2019年09月": "学生LT 第28回 運営&登壇",
+                "2019年04月": "新居浜高専 電気情報工学科に入学",
             }}
         </Table>
     </section>
