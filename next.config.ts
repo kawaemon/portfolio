79c38b94d060fdf7d82707bf739ef16b673bb0eb
kawaemon <me@kawaemon.dev>
2026-09-24 18:15:38 +0900

    webpack

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-    experimental: {
-        turbopackPluginRuntimeStrategy: "workerThreads",
-    },
-};
-
-export default nextConfig;
